bdbfbcc7f56192abe6ea455f2af88364939172a1
JoshEthan <joshuaeris@gmail.com>
2023-08-16 15:23:45 -0600

    Logging.

--- LegendCreator/src/LegendCreator.h
+++ LegendCreator/src/LegendCreator.h
@@ -3,6 +3,7 @@
 // For Legend Creator Apps
 #include <stdio.h>
 #include "LegendCreator/Application.h"
+#include "LegendCreator/Log.h"
 
 // ENTRY POINT
 #include "LegendCreator/EntryPoint.h"
---
+++ LegendCreator/src/LegendCreator/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "core.h"
+#include "spdlog/spdlog.h"
+
+namespace LegendCreator {
+
+	class LEGENDCREATOR_API Log
+	{
+	public:
+		static void init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger;  }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger;  }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core Log Macros
+#define LC_CORE_TRACE(...)	::LegendCreator::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define LC_CORE_INFO(...)	::LegendCreator::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define LC_CORE_WARN(...)	::LegendCreator::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define LC_CORE_ERROR(...)	::LegendCreator::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define LC_CORE_FATAL(...)	::LegendCreator::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client Log Macros
+#define LC_TRACE(...)		::LegendCreator::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define LC_INFO(...)		::LegendCreator::Log::GetClientLogger()->info(__VA_ARGS__)
+#define LC_WARN(...)		::LegendCreator::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define LC_ERROR(...)		::LegendCreator::Log::GetClientLogger()->error(__VA_ARGS__)
+#define LC_FATAL(...)		::LegendCreator::Log::GetClientLogger()->fatal(__VA_ARGS__)
